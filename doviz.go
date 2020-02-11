70c090a583bdc4f5b3f879db36a3039645b654f3
fakturk <f.akturk@gmail.com>
2020-02-12 00:58:11 +0300

    currency struct

---
+++ doviz.go
@@ -0,0 +1,20 @@
+package tcmb
+import ( 
+	"fmt"
+	"encoding/xml"
+)
+
+type currency struct {
+	Kod             string `xml:"Kod,attr"`
+	CrossOrder      string `xml:"CrossOrder,attr"`
+	CurrencyCode    string `xml:"CurrencyCode,attr"`
+	Unit            string `xml:"Unit"`
+	Isim            string `xml:"Isim"`
+	CurrencyName    string `xml:"CurrencyName"`
+	ForexBuying     string `xml:"ForexBuying"`
+	ForexSelling    string `xml:"ForexSelling"`
+	BanknoteBuying  string `xml:"BanknoteBuying"`
+	BanknoteSelling string `xml:"BanknoteSelling"`
+	CrossRateUSD    string `xml:"CrossRateUSD"`
+	CrossRateOther  string `xml:"CrossRateOther"`
+}
